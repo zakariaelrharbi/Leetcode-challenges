a079f389b6ec5909c7ca9fe2f6df66bb1985213a
zakariaelrharbi <zakariaelrharbi269@gmail.com>
2024-04-02 14:33:47 +0000

    second try

--- CounterII.js
+++ CounterII.js
@@ -28,3 +28,22 @@
 // counter.reset(); // 0
 // counter.reset(); // 0
  
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    return {
+        increment: function() {
+            return init = init + 1;
+        },
+
+        decrement: function() {
+            return init = init - 1;
+        }, 
+
+        reset: function() {
+            return init = 0;
+        }
+    };
+};
